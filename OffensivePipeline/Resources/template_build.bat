57af4f04692f13d52660533301627760a8462b25
brmkit <brmkit@protonmail.com>
2023-09-27 11:52:52 +0200

    optimized

--- OffensivePipeline/Resources/template_build.bat
+++ OffensivePipeline/Resources/template_build.bat
@@ -1,3 +1,3 @@
 call "{{MSBUILD_PATH}}"
-powershell.exe ..\..\Resources\retarget_framework.ps1
+powershell.exe -c "get-childItem -recurse -filter *.csproj | foreach { (get-content $_.FullName) -replace '<TargetFrameworkVersion>.*</TargetFrameworkVersion>', '<TargetFrameworkVersion>v4.6</TargetFrameworkVersion>' | set-content $_.FullName }"
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" "{{SOLUTION_PATH}}" {{BUILD_OPTIONS}} /p:OutputPath="{{OUTPUT_DIR}}" /p:DebugSymbols=false /p:DebugType=None /p:AssemblyName="{{OUTPUT_FILENAME}}"
