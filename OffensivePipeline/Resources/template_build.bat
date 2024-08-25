a43f3c239916079a852a327360aee807bedac4a5
brmk <brmkit@protonmail.com>
2024-08-25 16:46:15 +0200

    .

--- OffensivePipeline/Resources/template_build.bat
+++ OffensivePipeline/Resources/template_build.bat
@@ -1,3 +1,3 @@
 call "{{MSBUILD_PATH}}"
-powershell.exe -c "get-childItem -recurse -filter *.csproj | foreach { (get-content $_.FullName) -replace '<TargetFrameworkVersion>.*</TargetFrameworkVersion>', '<TargetFrameworkVersion>v4.6</TargetFrameworkVersion>' | set-content $_.FullName }"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" "{{SOLUTION_PATH}}" {{BUILD_OPTIONS}} /p:OutputPath="{{OUTPUT_DIR}}" /p:DebugSymbols=false /p:DebugType=None /p:AssemblyName="{{OUTPUT_FILENAME}}"
+powershell.exe -c "get-childItem -recurse -filter *.csproj | foreach { (get-content $_.FullName) -replace '<TargetFrameworkVersion>.*</TargetFrameworkVersion>', '<TargetFrameworkVersion>v4.7</TargetFrameworkVersion>' | set-content $_.FullName }"
+msbuild.exe "{{SOLUTION_PATH}}" {{BUILD_OPTIONS}} /p:OutputPath="{{OUTPUT_DIR}}" /p:DebugSymbols=false /p:DebugType=None /p:AssemblyName="{{OUTPUT_FILENAME}}"
